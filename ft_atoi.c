075d48d93082c93736580c6d8aeec2a85206b943
Justus Bauch <tarbrough@gmail.com>
2025-12-15 03:07:31 +0100

    added header to all files

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,41 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                         :+:      :+:    :+:    */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbauch <jbauch@student.42wolfsburg.de>      +#+  +:+       +#+       */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 02:26:00 by jbauch             #+#    #+#            */
+/*   Updated: 2025/12/15 02:26:00 by jbauch            ###   ########.fr      */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-int ft_char_forbidden(int i);
-
-int ft_atoi(const char *src){
-    int output;
-    int i;
-
-    output = 0;
-    int negpos = 1;
-    i = 0;
-
-    while (ft_char_forbidden(src[i]))
-        i++;
-
-    if (src[i] == '+' && src[i + 1] != '-')
-        i++;
-
-    if (src[i] == '-'){
-        negpos = -1;
-        i++;
-    }
-
-    while (src[i] && src[i] >= 48 && src[i] <= 57){
-        output *= 10;
-        output += src[i] -48;
-        i++;
-    }
-    output *= negpos;
-    return (output);
-
+int	ft_char_forbidden(int i)
+{
+	if (i >= 9 && i <= 13)
+		return (1);
+	if (i == 32)
+		return (1);
+	return (0);
 }
 
-int ft_char_forbidden(int i){
-    if (i >= 9 && i <= 13){
-        return(1);
-    } else if (i == 32){
-        return(1);
-    }
-    return (0);
+int	ft_atoi(const char *src)
+{
+	int	output;
+	int	i;
+	int	sign;
+
+	output = 0;
+	sign = 1;
+	i = 0;
+	while (ft_char_forbidden(src[i]))
+		i++;
+	if (src[i] == '+' && src[i + 1] != '-')
+		i++;
+	if (src[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (src[i] && src[i] >= '0' && src[i] <= '9')
+	{
+		output *= 10;
+		output += src[i] - '0';
+		i++;
+	}
+	return (output * sign);
 }
--- ft_calloc.c
+++ ft_calloc.c
@@ -1,23 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbauch <jbauch@student.42wolfsburg.de>      +#+  +:+       +#+       */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 02:31:00 by jbauch             #+#    #+#            */
+/*   Updated: 2025/12/15 02:31:00 by jbauch            ###   ########.fr      */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-void *ft_calloc(size_t count, size_t size)
+void    *ft_calloc(size_t count, size_t size)
 {
-    unsigned char *tmp;
-    size_t i;
-
+    unsigned char   *tmp;
+    size_t          i;
 
     if (count != 0 && size > INT_MAX / count)
         return (NULL);
-
-    i = 0;
-    /* allocating count * size bytes in memory with malloc */
     tmp = malloc(count * size);
-    /* check if the memory was allocated */
     if (!tmp)
         return (NULL);
-    /* loop over every allocated bytes and set it to 0 */
+    i = 0;
     while (i < count * size)
-        tmp[i++] = 0;
-    /* return the allocated memory */
+    {
+        tmp[i] = 0;
+        i++;
+    }
     return (tmp);
 }
