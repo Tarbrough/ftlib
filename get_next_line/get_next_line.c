5664bc168e4094315c2b98413bb8f26cbe98d590
Justus Bauch <tarbrough@gmail.com>
2026-02-14 01:42:04 +0100

    added files for nextline

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,37 @@
+#include "get_next_line.h"
+
+void	create_list(t_list **list, int fd)
+{
+	int		char_read;
+	char	*buf;
+	
+	while(!find_newline(*list))
+	{
+		buf = malloc(BUFFER_SIZE + 1);
+		if(!buf)
+			return;
+
+		char_read = read(fd, buf, BUFFER_SIZE);
+		if(!char_read)
+		{
+			free(buf);
+			return;
+		}
+		buf[char_read] = '\0';
+		append(list, buf);
+	}
+}
+
+char *get_next_line(int fd)
+{
+    static t_list	*list = NULL;
+	char			*next_line;
+
+	if(fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
+		return(NULL);
+
+	create_list(&list, fd);
+	if(list == NULL)
+		return(NULL);
+
+}
