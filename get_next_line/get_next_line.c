254b0c447c92aa028cacf4017062d5921047de69
Justus Bauch <tarbrough@gmail.com>
2026-02-14 01:51:27 +0100

    final changes for tonight

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,5 +1,37 @@
 #include "get_next_line.h"
 
+char	*get_line(t_list *list)
+{
+	int		len;
+	char	*next;
+
+	if (list == NULL)
+		return(NULL);
+
+	len = get_length(list);
+	next = malloc(len + 1)
+}
+
+void	append(t_list **list, char *buf)
+{
+	t_list *new;
+	t_list	*last;
+
+	last = find_last(*last);
+	new = malloc(sizeof(t_list));
+
+	if(!new)
+		return;
+
+	if(!last)
+		*list = new;
+	else
+		last->next = new;
+
+	new->buffer = buf;
+	new->next = NULL;
+}
+
 void	create_list(t_list **list, int fd)
 {
 	int		char_read;
@@ -34,4 +66,7 @@ char *get_next_line(int fd)
 	if(list == NULL)
 		return(NULL);
 
+	next_line = get_line(list);
+	clean_list(&list);
+	return(next_line);
 }
